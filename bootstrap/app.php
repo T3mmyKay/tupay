0b35b3a8b3f754e6cf4440b171b9cc104c60d5e3
Komolafe Temiloluwa Ayomide <103121606+T3mmyKay@users.noreply.github.com>
2026-07-31 22:00:18 +0100

    fix(api): normalize Laravel throttle exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,11 +17,11 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
 use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -125,7 +125,7 @@ return Application::configure(basePath: dirname(__DIR__))
                     'FX rate unavailable',
                     $exception->getMessage(),
                 ),
-                $exception instanceof TooManyRequestsHttpException => ProblemDetails::response(
+                $exception instanceof ThrottleRequestsException => ProblemDetails::response(
                     $request,
                     429,
                     'RATE_LIMIT_EXCEEDED',
